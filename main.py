3fd0ac347d561f8acf45da2d6b205c03905fcf9a
t0tal <total.prakash@gmail.com>
2014-12-03 03:18:40 -0500

    deleted key for security

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ from amazon_scraper import AmazonScraper
 import warnings
 from bs4 import BeautifulSoup
 import time
+from xlrd import open_workbook
+from xlwt import Workbook               #packages for working with excel files
+import xlutils
+from xlrd.sheet import Sheet
+import itertools
 
-#Global variables and settings
+#Global defaults
 warnings.filterwarnings("ignore")
 AWS_ACCESS_KEY_ID = "AKIAIETM24OXNYX6EKAQ"
 AWS_SECRET_ACCESS_KEY = "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"
 AWS_ASSOCIATE_TAG = "personaltwi08-20"
 
-
+#adds variable headers to the first row of the given sheet
+def add_data_headers(sheet):
+    data_headers = ('data_id','amazon_id','product_name', 'product_type', 'category', 'sub_category', 'sales_rank', 'URL')
+    for c in range(len(data_headers)):
+        sheet.write(0,c,data_headers[c])
 
 #This function takes in a product object and returns the # of 1-5 star ratings as a list
 def ratings(product):
@@ -53,25 +62,48 @@ def ratings(product):
 #Main function of this program
 
 def main():
-    #Create Scraper
-    a = AmazonScraper(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_ASSOCIATE_TAG)
+    #user settings
+    input_file_name = 'data/raw_data_1203.xlsx'
+    output_file_name = 'data/output_data.xls'
+    input_sheet_name = 'raw_data'
+    output_sheet_name = 'processed_data'
     
-    #Get list of items from excel file
+
+#     #Initialize from given settings
+#     book_in = open_workbook(input_file_name)
+#     sheet_in = book_in.sheet_by_name(input_sheet_name)
+#     book_out = Workbook()
+#     sheet_out = book_out.add_sheet(output_sheet_name)
+#     add_data_headers(sheet_out)
+#     
+#     #Get list of items from excel file
+#     url_list = sheet_in.col_values(8,1)
     
     
-    #Look up required item
-    p = a.lookup(ItemId='B0014IKQKG')
+    amzn = AmazonScraper(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_ASSOCIATE_TAG)    
+    for p in itertools.islice(amzn.search(Keywords='toasters',SearchIndex='Kitchen'), 5):
+        print p.title
+    
+#     for url in url_list:
+#         p = amzn.lookup(URL=url)
+        
     
     
-    for i in range(0,3):
-        rs = a.reviews(URL=p.reviews_url)
-        if(len(rs.ids) == 0):
-            time.sleep(3)
-        else:
-            break
-    r = a.review(Id=rs.ids[0])
+    #Saving the workbook
+#     book_out.save(output_file_name)
     
-    print r.text
+    #p = amzn.lookup(ItemId='B0014IKQKG')
+    
+    
+#     for i in range(0,3):
+#         rs = amzn.reviews(URL=p.reviews_url)
+#         if(len(rs.ids) == 0):
+#             time.sleep(3)
+#         else:
+#             break
+#     r = amzn.review(Id=rs.ids[0])
+#     
+#     print r.text
     
     #print(ratings(p))
 
