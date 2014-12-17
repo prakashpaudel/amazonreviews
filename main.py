b93e204b97f00f0690d9db2fbca4c858623562c3
t0tal <total.prakash@gmail.com>
2014-12-16 20:26:58 -0500

    going through

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ from types import NoneType
 #Global defaults
 warnings.filterwarnings("ignore")
 
+def releaseDate(p):
+    date = p.product._safe_get_element_text('SellerListing.StartDate')
+    print date
 
+def isPrime(p):
+    isPrime = p.product._safe_get_element_text('Offers.Offer.OfferListing.IsEligibleForSuperSaverShipping')
+    return int(isPrime) if isPrime else 0
 
 def avg_rating(p_ratings):
     return (p_ratings[0]*1 + p_ratings[1]*2 + p_ratings[2]*3 + p_ratings[3]*4 + p_ratings[4]*5)/(total_ratings(p_ratings)+0.0)
@@ -25,83 +31,60 @@ def total_ratings(p_ratings):
 
 #This function takes in a product object and returns the rank of it as an string
 def rank(p):
-    rank = p.__getattr__('sales_rank')
+    rank = p.product.sales_rank
     return int(rank) if rank else 0
-
-def price(p):
-    return p.price_and_currency[0]
-
-def list_price(p):
-    return p.product.list_price[0]
 
 def add_data(sheet, count, data_list):
     for i in range(len(data_list)):
         sheet.write(count, i, data_list[i])
 
-
-#adds variable headers to the first row of the given sheet
-def add_data_headers(sheet):
-    data_headers = ('a_id','name', 'type', 'category', 'rank', 'price', 'stars_1', 'stars_2', 'stars_3', 'stars_4', 'stars_5', 'rating', 'total_reviews', 'url')
-    for c in range(len(data_headers)):
-        sheet.write(0,c,data_headers[c])
-
 #This function takes in a product object and returns the # of 1-5 star ratings as a list
 def ratings(amzn, p):
     ratings = [0, 0, 0, 0, 0]
-    has_reviews, reviews_url = p.product.reviews
+    has_reviews = p.product.reviews[0]
     if has_reviews:
         rs = amzn.reviews(URL=p.reviews_url)
-        soup = rs.soup
-        div = soup.find("div","class":"crIFrameHeaderHistogram")
-        if div:
-            table = div.find('table')
+        rs_soup = rs.soup
+        summary = rs_soup.find('table', id='productSummary')
+        if summary:
+            table = summary.find('table')
             if table:
-                
-        
+                for rating, row in zip([4,3,2,1,0], table.find_all('tr')):
+                    children = [child for child in row.find_all('td', recursive=False)]
+                    td = children[2]
+                    value = td.string
+                    value = value[2:-1]
+                    value = value.replace(',', '')
+                    ratings[rating] = int(value)
     return ratings
-    
-    
-    
-    
-    
-#     table = product.soup.find('table', id='histogramTable')
-#     if table:
-#         for rating, row in zip([4,3,2,1,0], table.find_all('tr', class_='a-histogram-row')):
-#             # get the third td tag
-#             children = [child for child in row.find_all('td', recursive=False)]
-#             td = children[2]
-#             data = td.find('a')
-#             if data:
-#                 # number could have , in it which fails during int conversion
-#                 value = data.string
-#                 value = value.replace(',', '')
-#                 ratings[rating] = int(value)
-#         return ratings
-#     
-#     reviews_div = product.soup.find('div', class_='reviews')
-#     if reviews_div:
-#         for rating, rating_class in [
-#             (4, 'histoRowfive'),
-#             (3, 'histoRowfour'),
-#             (2, 'histoRowthree'),
-#             (1, 'histoRowtwo'),
-#             (0, 'histoRowone'),
-#         ]:
-#             rating_div = reviews_div.find('div', class_=rating_class)
-#             if rating_div:
-#                 # no ratings means this won't exist
-#                 tag = rating_div.find('div', class_='histoCount')
-#                 if tag:
-#                     value = tag.string
-#                     value = value.replace(',', '')
-#                     ratings[rating] = int(value)
-#         return ratings
-#     
-#     return ratings
+
+def data(amzn, p, p_type, p_category):
+    p_name = p.title
+    p_id = p.asin
+    p_ratings = ratings(amzn, p)
+    p_total_ratings = total_ratings(p_ratings)
+    p_avg_rating = 0 if p_total_ratings == 0 else avg_rating(p_ratings)
+    p_price = p.product.price_and_currency[0]
+    p_list_price = p.product.list_price[0]
+    p_rank = rank(p)
+    p_url = p.url
+    p_isPrime = isPrime(p)
+    p_releaseDate = releaseDate(p)
+    return [p_type, p_category, p_id, p_name, p_rank, p_price, p_list_price, p_isPrime,
+            p_ratings[0], p_ratings[1], p_ratings[2], p_ratings[3], p_ratings[4],
+            p_avg_rating, p_total_ratings,
+            p_url]
+
+#adds variable headers to the first row of the given sheet
+def add_data_headers(sheet):
+    data_headers = ('type', 'category', 'a_id','name', 'rank', 'price', 'list_price', 'prime',
+                    'stars_1', 'stars_2', 'stars_3', 'stars_4', 'stars_5', 'rating', 'num_reviews',
+                    'url')
+    for c in range(len(data_headers)):
+        sheet.write(0,c,data_headers[c])
 
 
 #Main function of this program
-
 def main():
     #user settings
     input_file_name = 'data/input.xlsx'
@@ -109,7 +92,7 @@ def main():
     input_sheet_name = 'product_list'
     output_sheet_name = 'processed_data'
     
-    number_of_items = 2
+    number_of_items = 5
 
     #Initialize from given settings
     book_in = open_workbook(input_file_name)
@@ -131,32 +114,8 @@ def main():
         for p in itertools.islice(amzn.search(Keywords=product_type_list[i],SearchIndex=search_index_list[i]), number_of_items):
             p_count += 1
             print 'Processing', product_type_list[i], p_count
-            p_name = p.title
-            p_id = p.asin
-            p_ratings = ratings(amzn, p)
-            p_total_ratings = total_ratings(p_ratings)
-            p_avg_rating = 0 if p_total_ratings == 0 else avg_rating(p_ratings)
-            p_price = price(p)
-            p_list_price = list_price(p)
-            p_rank = rank(p)
-            p_url = p.url
-            add_data(sheet_out, p_count, [p_id, p_name, product_type_list[i], search_index_list[i], p_rank, p_price, p_ratings[0], p_ratings[1], p_ratings[2], p_ratings[3], p_ratings[4], p_avg_rating, p_total_ratings, p_url])
-            book_out.save(output_file_name + '_' + product_type_list[i] + '.xls') 
-        
-            
-         
-    
-    
-#     rs_all = []
-#     rs_url = p.reviews_url
-#     for i in range(0,3):
-#         page = amzn.reviews(URL=rs_url)
-#         time.sleep(3)
-#         if(len(page.ids) != 0):
-#             break
-        
-    #iterating through all reviews
-#     for r_id in rs:
-#         print r_id
+            p_data = data(amzn, p, product_type_list[i], search_index_list[i])
+            add_data(sheet_out, p_count, p_data)
+            book_out.save(output_file_name + '_' + product_type_list[i] + '.xls')
     
 main()
